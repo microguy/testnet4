19d9f79fc9f327ce3e4b42466dbff315bc2c0622
Greg Matthews <webmaster@microguy.net>
2023-08-14 12:03:11 -0700

    Update MAX_MONEY
    This is required to allow for the super block transaction in which the new treasury will be funded.

--- src/amount.h
+++ src/amount.h
@@ -31,7 +31,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 84000000 * COIN;
+static const CAmount MAX_MONEY = 20000000000 * COIN; // Treasury Fork: max transaction 20,000,000,000 coins
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
